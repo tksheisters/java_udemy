87f15e0070fccbe08dc2df833061fb04d0ce8753
Matheus André <tksheisters@gmail.com>
2023-04-12 17:19:37 -0300

    .

---
+++ Programa12/src/programa12/Main.java
@@ -0,0 +1,31 @@
+package programa12;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		// && = E, || = OU, ! = negação
+
+		// estrutura condicional simples
+		System.out.println("Quantas horas? ");
+		int x = sc.nextInt();
+
+		if (x < 12 && x > 5) {
+			System.out.println("Bom dia");
+
+		} else {
+			if (x >= 12 && x <= 18) {
+				System.out.println("Boa tarde");
+
+			} else {
+				System.out.println("Boa noite");
+
+			}
+
+		}
+
+	}
+}
