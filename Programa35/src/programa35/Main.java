7cbd8d7e2c4498159c54747bfc819d82976f4cee
Matheus André <tksheisters@gmail.com>
2023-04-13 19:24:39 -0300

    .

--- Programa35/src/programa35/Main.java
+++ Programa35/src/programa35/Main.java
@@ -13,16 +13,12 @@ public class Main {
 		do {
 			System.out.print("Digite a temperatura em Celsius: ");
 			temp = sc.nextDouble();
-			
 			temp = 9 * temp / 5 + 32;
-			
-			sc.nextLine();
 			
 			System.out.printf("Farenheit: %.1fF\n", temp);
 			
 			System.out.print("Deseja repetir (s/n)? ");
 			repete = sc.next().charAt(0);
-			
 		} while (repete == 's' || repete == 'S');
 		
 		sc.close();
