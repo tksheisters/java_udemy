be2822129f848c5a4751e4ab08b3c14c4419874f
Matheus André <tksheisters@gmail.com>
2023-04-14 18:42:34 -0300

    .

--- Programa43/src/application/Program.java
+++ Programa43/src/application/Program.java
@@ -1,10 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		Product product = new Product();
+		System.out.println("Enter product data:");
+		
+		System.out.print("Name: ");
+		product.name = sc.next().toUpperCase();
+
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+
+		System.out.print("Quantity: ");
+		product.quantity = sc.nextInt();
+
+		double total = product.totalValueStock();
+
+		System.out.printf("Product data: \nName: %s\nPrice: %.2f\nQuantity: %s\nTotal: ", product.name, product.price,
+				product.quantity, total);
+
+		sc.close();
 	}
 
 }
---
+++ Programa43/src/entities/Product.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueStock() {
+		double value = price * quantity;
+		return value;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+}
