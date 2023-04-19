5fb4f4d8f8ea6e107708313d3c52539cbf4f31d2
Matheus André <tksheisters@gmail.com>
2023-04-19 17:37:52 -0300

    .

--- Programa53/src/application/Main.java
+++ Programa53/src/application/Main.java
@@ -1,18 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int x, y;
-		x = 10;
-		y = x;
 
-		x += 2;
-		y += 1;
+		Scanner input = new Scanner(System.in);
 
-		System.out.println(x);
-		System.out.println(y);
+		System.out.print("Tamanho do vetor: ");
+		int n = input.nextInt();
+		double[] vect = new double[n];
 
+		for (int i = 0; i < vect.length; i++) {
+			System.out.print("Altura: ");
+			vect[i] = input.nextDouble();
+		}
+
+		double soma = 0.0;
+		for (int i = 0; i < vect.length; i++) {
+			soma += vect[i];
+		}
+
+		double media = soma / vect.length;
+
+		System.out.printf("Media das alturas: %.2f", media);
+
+		input.close();
 	}
 
 }
