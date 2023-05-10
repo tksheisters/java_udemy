99d8726bc9dfaec33bf8b9e3eac40ceb20e35969
Matheus Andre <tksheisters@gmail.com>
2023-05-10 15:01:22 -0300

    .

---
+++ composicaoObjeto02/src/application/App.java
@@ -0,0 +1,61 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
+public class App {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter client data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine().toUpperCase();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println();
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next().toUpperCase());
+		Order order = new Order(new Date(), status, client);
+		
+		System.out.print("How many itens to this order?: ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Enter #%d item data: \n", (i+1));
+			System.out.print("Product name: ");
+			String productName = sc.next().toUpperCase();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			Product product = new Product(productName, productPrice);
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+			OrderItem orderItem = new OrderItem(quantity, product.getPrice(), product);
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		
+		System.out.println(order);
+		
+		
+		sc.close();
+
+	}
+
+}
