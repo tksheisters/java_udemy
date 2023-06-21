94ae48c576c0557bd2ab5a7d552904e06b341589
Matheus <tksheisters@gmail.com>
2023-06-21 11:51:07 -0300

    tipos super coringa

---
+++ generics03/src/application/App.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+	public static void main(String[] args) {
+		// List<Object> myObjs = new ArrayList<Object>(); um list<integer> n é um
+		// subtipo de list<object>
+		List<?> myObjs = new ArrayList<Object>(); // supertipo de qualquer tipo de lista
+		List<Integer> myNumbers = new ArrayList<Integer>();
+		myObjs = myNumbers;
+
+		// aqui sim integer é um subtipo de object
+		Object obj;
+		Integer x = 10;
+		obj = x;
+
+		Integer[] numbers = { 5, 2, 10 };
+		List<Integer> myInts = new Arrays.asList(numbers);
+		printList(myInts);
+	}
+
+	public static void printList(List<?> list) {
+		for (Object object : list) {
+			System.out.println(object);
+		}
+	}
+
+}
