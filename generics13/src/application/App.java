3d74c5e7a7deaed4bc74e4b5fbb51f7b5fd09054
Matheus <tksheisters@gmail.com>
2023-07-06 12:13:10 -0300

    exercicio resolvido sobre map

--- generics13/src/application/App.java
+++ generics13/src/application/App.java
@@ -2,6 +2,9 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -13,15 +16,30 @@ public class App {
 		System.out.print("Enter file path: ");
 		String path = sc.nextLine();
 
+		Map<String, Integer> votosPorNome = new HashMap<>();
+
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
 			String line = br.readLine();
 			while (line != null) {
 				String[] fields = line.split(",");
-				System.out.println(fields);
+				
+				String nome = fields[0];
+				int votos = Integer.parseInt(fields[1]);
+
+				if (votosPorNome.containsKey(nome)) {
+					votos += votosPorNome.get(nome);
+				}
+				
+				votosPorNome.put(nome, votos);
+				
 				line = br.readLine();
 			}
-		} catch (Exception e) {
-			// TODO: handle exception
+			for (Map.Entry<String, Integer> entry : votosPorNome.entrySet()) {
+				System.out.println(entry.getKey().toUpperCase() + ": " + entry.getValue());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 		sc.close();
